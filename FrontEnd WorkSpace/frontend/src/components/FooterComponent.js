a1bdfe7f5b0fd896aa384b5c33022873ed56cd46
LAPTOP-JGK1I4TE\Lenovo <manojkumardoddula456@gmail.com>
2025-02-03 14:43:00 +0530

    Employee Management System Project

---
+++ FrontEnd WorkSpace/frontend/src/components/FooterComponent.js
@@ -0,0 +1,10 @@
+import '../App.css';
+function FooterComponent() 
+{
+    return(
+        <footer className='footer'>
+            <div> All Rights Are Reserved &copy; 2025 by Manoj Kumar</div> 
+        </footer>
+    )
+}
+export default FooterComponent;
---
+++ FrontEnd WorkSpace/frontend/src/components/HeaderComponent.js
@@ -0,0 +1,17 @@
+
+function HeaderComponent()
+{
+    return(
+        <header>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+            <a href="" className="navbar-brand">
+                Employee Management System
+            </a>    
+            </div>
+        </nav>
+        </header>
+
+    )
+}
+export default HeaderComponent;
